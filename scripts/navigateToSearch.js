3473e32c5bcb132b4ec33d5689f0768b4c0fd340
mason young <masonyoung1997@gmail.com>
2025-03-24 10:15:17 -0700

    Make item search case agnostic

--- scripts/navigateToSearch.js
+++ scripts/navigateToSearch.js
@@ -1,8 +1,15 @@
+function titleCase(item) {
+    item = item.toLowerCase().split(' ').map(function (word) {
+        return word.replace(word[0], word[0].toUpperCase());
+    });
+    return item.join(' ');
+}
 function navigateToSearchPage(collection) {
 
     $("#searchButton").click(function () {
         let searchName = $('input[name="item"]').val()
-        localStorage.setItem('searchItem', searchName);
+        let searchTitleCase = titleCase(searchName)
+        localStorage.setItem('searchItem', searchTitleCase);
         // handle clicking search button
         window.location.href = './search.html';
 
@@ -13,7 +20,8 @@ function navigateToSearchPage(collection) {
         if (e.keyCode == 13) {
             e.preventDefault();
             let searchName = $('input[name="item"]').val()
-            localStorage.setItem('searchItem', searchName);
+            let searchTitleCase = titleCase(searchName)
+            localStorage.setItem('searchItem', searchTitleCase);
             window.location.href = './search.html';
         }
     })
