91d59b827c88b046c0aa4247cf94984af1d898a0
Quinn DM <qdesforgemajor@my.bict.ca>
2025-04-06 16:27:33 -0700

    Clean up code in search.js

--- scripts/search.js
+++ scripts/search.js
@@ -1,26 +1,26 @@
-var currentUser;
-let filter_open = false;
+var currentUser; // Get user
+let filter_open = false; // Set filter to closed by default
 
+// Convert item title to title case
 function titleCase(item) {
     item = item.toLowerCase().split(' ').map(function (word) {
         return word.replace(word[0], word[0].toUpperCase());
     });
     return item.join(' ');
 }
+
 // Function to read URL parameters
 function getURLParameter(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
 }
 
-
-// Function that calls everything needed for the deals page
+// Call everything needed for the deals page
 function doAll() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            currentUser = db.collection("users").doc(user.uid); // global
+            currentUser = db.collection("users").doc(user.uid);
             console.log(currentUser);
-
             // Handle URL search parameter
             const itemParam = getURLParameter("item");
             if (itemParam) {
@@ -75,9 +75,6 @@ function doAll() {
             $(".choices_markets").click(function () {
                 displayCardsDynamically("deals");
             });
-            // $(".whole_foods").click(function () {
-            //     displayCardsDynamically("deals");
-            // });
             $(".min-price").change(function () {
                 displayCardsDynamically("deals");
             });
@@ -90,21 +87,9 @@ function doAll() {
             $(".filter_enddate").change(function () {
                 displayCardsDynamically("deals");
             });
-            // $("#open_close").click(function () {
-            //     $("#filter_contents").toggle();  // Toggles visibility of the element
-            //     if (filter_open == false) {
-            //         filter_open = true;
-            //         $("#open_close").html("<div id='open_close' class='text - center'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' width='30px' class='size-6'><path stroke-linecap='round' stroke-linejoin='round' d='m19.5 8.25-7.5 7.5-7.5-7.5' /></svg><div>")
-            //     } else {
-            //         filter_open = false
-            //         $("#open_close").html("<div id='open_close' class='text - center'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' width='30px' class='size-6'><path stroke-linecap='round' stroke-linejoin='round' d='m4.5 15.75 7.5-7.5 7.5 7.5' /></svg></div>")
-            //     }
-            // });
             $("#toggleSidebar").click(function () {
-                $(".sidebar").toggle(); // Toggle sidebar
+                $(".sidebar").toggle();
             });
-
-
             populateSearchBoxValue();
             colourActiveFilter();
             displayCardsDynamically("deals");
@@ -116,28 +101,25 @@ function doAll() {
         }
     });
 }
-
 doAll();
-// Function to read URL parameters
+
+// Read URL parameters
 function getURLParameter(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
 }
 
-// Check if we have an item parameter in the URL
+// Check for item parameter in the URL
 document.addEventListener("DOMContentLoaded", function () {
     const itemParam = getURLParameter("item");
-
     if (itemParam) {
         // Set the search box value to the item parameter
         const searchBox = document.getElementById("searchBox");
         if (searchBox) {
             const decodedItem = decodeURIComponent(itemParam);
             searchBox.value = decodedItem;
-
             // Also store in localStorage for the search functionality
             localStorage.setItem("searchItem", decodedItem);
-
             // Trigger the search - either by clicking the button or calling the search function
             const searchButton = document.getElementById("searchButton");
             if (searchButton) {
@@ -150,12 +132,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-
-// This is to control how the filter bar behaves when moving between small and larger screen sizes
+// Control how the filter bar behaves when moving between small and large screens
 document.addEventListener("DOMContentLoaded", function () {
     function updateOffcanvas() {
         const offcanvasElement = document.getElementById("offcanvasTop");
-
         if (window.innerWidth < 768) { // Small screens
             offcanvasElement.classList.remove("offcanvas-start");
             offcanvasElement.classList.add("offcanvas-top");
@@ -171,8 +151,7 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("resize", updateOffcanvas);
 });
 
-
-// handle filtering the deals
+// Handle filtering the deals
 filterActivated = false;
 filterField = "price";
 function filterDeals(filter) {
@@ -204,7 +183,6 @@ function formatEndDateForDatabase(endDate) {
 function addWhereClauses(collection) {
     let searchNameGet = localStorage.getItem("searchItem");
     let search = db.collection(collection).where("itemName", "==", searchNameGet);
-
     // Retailer
     let retailer = [];
     if ($(".no_frills").is(":checked")) {
@@ -226,7 +204,6 @@ function addWhereClauses(collection) {
     if (retailer.length > 0) {
         search = search.where("retailer", "in", retailer);
     }
-
     // Price
     let min = $(".min-price").val() ? parseFloat($(".min-price").val()) : 0;
     let max = $(".max-price").val() ? parseFloat($(".max-price").val()) : Infinity;
@@ -236,13 +213,11 @@ function addWhereClauses(collection) {
     if ($(".max-price").val()) {
         search = search.where("price", "<=", max);
     }
-
     // Brands
     if (brand_to_filter_by = $(".filter_brand").val()) {
         brand_to_filter_by_title_case = titleCase(brand_to_filter_by)
         search = search.where("brand", "==", brand_to_filter_by_title_case);
     }
-
     // End Date
     if (enddate_to_filter_by = $(".filter_enddate").val()) {
         let endDate = new Date(enddate_to_filter_by)
@@ -251,15 +226,14 @@ function addWhereClauses(collection) {
         console.log(firestoreEndDate)
         search = search.where("expiryDate", ">", firestoreEndDate);
     }
-
     $("#deals-go-here").empty();
     return search
 }
 
 
 function displayCardsDynamically(collection) {
-    let cardTemplate = document.getElementById("dealCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable.
-    let searchNameGet = localStorage.getItem("searchItem");
+    // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable.
+    let cardTemplate = document.getElementById("dealCardTemplate");
     if (filterActivated == false) {
         dbCollection = addWhereClauses(collection)
     } else if (filterActivated == true) {
@@ -275,27 +249,24 @@ function displayCardsDynamically(collection) {
         } else {
             $("#deals-container").attr("style", "display: flex !important;");
         }
-        var i = 1; //Optional: if you want to have a unique ID for each hike
+        var i = 1;
         allDeals.forEach((doc) => {
-            //iterate thru each doc
-            var title = doc.data().name; // get value of the "name" key
+            // Iterate through each doc
+            var title = doc.data().name;
             var deal = doc.data().deal;
-            console.log(title);
             var price = doc.data().price;
             var imageCode = doc.data().code;
-            var dealStartDate = doc.data().startDate; // get value of the "details" key   //get unique ID to each hike to be used for fetching right image //gets the length field
             var dealEndDate = doc.data().endDate;
             var retailer = doc.data().retailer;
-            var UPC = doc.data().UPC;
-            let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
-            //update title and text and image
+            // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+            let newcard = cardTemplate.content.cloneNode(true);
+            // Update title and text and image
             newcard.querySelector(".card-item").innerHTML = title;
             newcard.querySelector(".card-deal").innerHTML = deal;
             newcard.querySelector(".card-price").innerHTML = `$${price}`;
             newcard.querySelector(".card-end-date").innerHTML = dealEndDate;
             newcard.querySelector(".card-retailer").innerHTML = retailer;
-            newcard.querySelector(".card-image").src = `./images/items/${imageCode}`; //`.images/items/${imageCode}`; //Example: NV01.jpg `./images/${imageCode}.png`
-
+            newcard.querySelector(".card-image").src = `./images/items/${imageCode}`;
             // Assign a unique ID to the cloned card
             let cardElement = newcard.querySelector(".card");
             if (cardElement) {
@@ -316,30 +287,29 @@ function displayCardsDynamically(collection) {
     });
 }
 
+// Display deal popup if user clicks on a deal card
 function displayDealPopupsDynamically(collection) {
-    let popupTemplate = document.getElementById("dealPopupTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable.
+    let popupTemplate = document.getElementById("dealPopupTemplate");
     let searchNameGet = localStorage.getItem("searchItem");
-    let searchName = $('input[name="item"]').val();
     db.collection(collection)
         .where("itemName", "==", searchNameGet)
         .get()
         .then((allDeals) => {
-            var i = 1; //Optional: if you want to have a unique ID for each hike
+            var i = 1;
             allDeals.forEach((doc) => {
-                //iterate thru each doc
+                //iterate through each doc
                 var description = doc.data().description;
-                var title = doc.data().name; // get value of the "name" key
+                var title = doc.data().name;
                 var deal = doc.data().deal;
                 var imageCode = doc.data().code;
                 var price = doc.data().price;
                 var brand = doc.data().brand;
-                var dealStartDate = doc.data().startDate; // get value of the "details" key   //get unique ID to each hike to be used for fetching right image //gets the length field
                 var dealEndDate = doc.data().endDate;
                 var retailer = doc.data().retailer;
-                var UPC = doc.data().UPC;
                 var retailerURL = doc.data().retailerURL;
                 var docID = doc.id;
-                let newcard = popupTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                let newcard = popupTemplate.content.cloneNode(true);
                 //update title and text and image
                 newcard.querySelector('.card-description').innerHTML = description;
                 newcard.querySelector(".card-item").innerHTML = title;
@@ -351,14 +321,12 @@ function displayDealPopupsDynamically(collection) {
                 newcard.querySelector(
                     ".card-image-popup"
                 ).src = `./images/items/${imageCode}`;
-
                 let popupElement = newcard.querySelector(".popup");
                 if (popupElement) {
                     popupElement.setAttribute("id", "popup_" + doc.id);
                     popupElement.style.display = "none";
                     popupElement.classList.add(i);
                 }
-
                 newcard
                     .querySelector(".saveDealButton")
                     .setAttribute("id", "saveDeal" + i);
@@ -380,11 +348,10 @@ function displayDealPopupsDynamically(collection) {
                         }
                     });
                 }
-                //attach to gallery, Example: "hikes-go-here"
                 document
                     .getElementById(collection + "-popup-here")
                     .appendChild(newcard);
-                i++; //Optional: iterate variable to serve as unique ID
+                i++; // Iterate variable to serve as unique ID
                 $(".btn-close").click(function () {
                     $("#deals-popup-here").hide();
                     $("#overlay").hide();
@@ -394,51 +361,21 @@ function displayDealPopupsDynamically(collection) {
         });
 }
 
-// This ensures that the search item name remains in the search box after search
+// Ensure that search item name remains in the search box after search
 function populateSearchBoxValue() {
     let searchNameGet = localStorage.getItem("searchItem");
     $("#searchBox").val(searchNameGet);
 }
 
-// $(document).ready(function () {
-//     $("#price").click(function () {
-//         filterDeals("price");
-//         displayCardsDynamically("deals");
-//     });
-//     $("#discount").click(function () {
-//         filterDeals();
-//         displayCardsDynamically("deals");
-//     });
-//     $("#newest").click(function () {
-//         filterDeals();
-//         displayCardsDynamically("deals");
-//     });
-//     $("#enddate").click(function () {
-//         filterDeals("endDate");
-//         displayCardsDynamically("deals");
-//     });
-//     $("#retailer").click(function () {
-//         filterDeals("retailer");
-//         displayCardsDynamically("deals");
-//     });
-//     $("#brand").click(function () {
-//         filterDeals("brand");
-//         displayCardsDynamically("deals");
-//     });
-//     populateSearchBoxValue();
-//     colourActiveFilter();
-//     displayCardsDynamically("deals");
-//     displayDealPopupsDynamically("deals");
-// });
-
+// Save/unsave bookmarks
 function saveBookmark(dealDocID) {
     let iconID = 'save-' + dealDocID;
     let bookmarkIcon = document.getElementById(iconID);
-
     currentUser.get().then(userDoc => {
+        // Check if user is logged in
         if (userDoc.exists) {
             let bookmarks = userDoc.data().bookmarks || [];
-
+            // Check if deal is bookmarked
             if (bookmarks.includes(dealDocID)) {
                 // If the deal is already bookmarked, remove it
                 currentUser.update({
@@ -471,7 +408,6 @@ let mybutton = document.getElementById("scrollToTopButton");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
-
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
